b1dac972998a2d7d083f3ac36523f95ff911a6c5
0xRh4ps00dy <marcosjurado83@gmail.com>
2024-09-19 12:28:45 +0200

    vault backup: 2024-09-19 12:28:45

---
+++ HTML import/Attachments/admin.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Acceder < Marcos Jurado — WordPress</title>
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='dns-prefetch' href='//i0.wp.com' />
+<link rel='dns-prefetch' href='//c0.wp.com' />
+<link rel='dns-prefetch' href='//www.googletagmanager.com' />
+<link rel='stylesheet' id='dashicons-css' href='https://c0.wp.com/c/6.6.2/wp-includes/css/dashicons.min.css' media='all' />
+<link rel='stylesheet' id='buttons-css' href='https://c0.wp.com/c/6.6.2/wp-includes/css/buttons.min.css' media='all' />
+<link rel='stylesheet' id='forms-css' href='https://c0.wp.com/c/6.6.2/wp-admin/css/forms.min.css' media='all' />
+<link rel='stylesheet' id='l10n-css' href='https://c0.wp.com/c/6.6.2/wp-admin/css/l10n.min.css' media='all' />
+<link rel='stylesheet' id='login-css' href='https://c0.wp.com/c/6.6.2/wp-admin/css/login.min.css' media='all' />
+<meta name="generator" content="Site Kit by Google 1.135.0" /><meta name="google-site-verification" content="sX4KG0vlrB1yxkTknOIoXAPCRpSx8HkvPJoq1Ym-PKQ">	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://i0.wp.com/marcosjurado.com/wp-content/uploads/2024/03/cropped-Icono.png?fit=32%2C32&#038;ssl=1" sizes="32x32" />
+<link rel="icon" href="https://i0.wp.com/marcosjurado.com/wp-content/uploads/2024/03/cropped-Icono.png?fit=192%2C192&#038;ssl=1" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://i0.wp.com/marcosjurado.com/wp-content/uploads/2024/03/cropped-Icono.png?fit=180%2C180&#038;ssl=1" />
+<meta name="msapplication-TileImage" content="https://i0.wp.com/marcosjurado.com/wp-content/uploads/2024/03/cropped-Icono.png?fit=270%2C270&#038;ssl=1" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-es-es">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+		<div id="login">
+		<h1><a href="https://es.wordpress.org/">Funciona con WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://marcosjurado.com/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nombre de usuario o correo electrónico</label>
+				<input type="text" name="log" id="user_login" class="input" value="marcosjurado" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Contraseña</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostrar la contraseña">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Recuérdame</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
+									<input type="hidden" name="redirect_to" value="https://marcosjurado.com/wp-admin/admin.php?page=stats&amp;noheader&amp;proxy&amp;chart=admin-bar-hours-scale" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://marcosjurado.com/wp-login.php?action=lostpassword">¿Has olvidado tu contraseña?</a>			</p>
+			<script>
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_pass" ); d.value = "";d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+</script>
+		<p id="backtoblog">
+			<a href="https://marcosjurado.com/">&larr; Ir a Marcos Jurado</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">
+							Idioma						</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="es_ES" lang="es" selected='selected' data-installed="1">Español</option></select>
+					
+											<input type="hidden" name="redirect_to" value="https://marcosjurado.com/wp-admin/admin.php?page=stats&noheader&proxy&chart=admin-bar-hours-scale" />
+					
+					
+						<input type="submit" class="button" value="Cambiar">
+
+					</form>
+				</div>
+				<link rel='stylesheet' id='jetpack_css-css' href='https://marcosjurado.com/wp-content/plugins/jetpack/css/jetpack.css?ver=13.8' media='all' />
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/jquery/jquery.min.js" id="jquery-core-js"></script>
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/jquery/jquery-migrate.min.js" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"https:\/\/marcosjurado.com\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/zxcvbn-async.min.js" id="zxcvbn-async-js"></script>
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/dist/hooks.min.js" id="wp-hooks-js"></script>
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/dist/i18n.min.js" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Fortaleza de la contrase\u00f1a desconocida","short":"Muy d\u00e9bil","bad":"D\u00e9bil","good":"Medio","strong":"Fuerte","mismatch":"No coinciden"};
+</script>
+<script src="https://c0.wp.com/c/6.6.2/wp-admin/js/password-strength-meter.min.js" id="password-strength-meter-js"></script>
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/underscore.min.js" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src="https://c0.wp.com/c/6.6.2/wp-includes/js/wp-util.min.js" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"35a35dac94"};
+</script>
+<script src="https://c0.wp.com/c/6.6.2/wp-admin/js/user-profile.min.js" id="user-profile-js"></script>
+	</body>
+	</html>
+	
